831ea3aa49d63254d9604d35c35dac53acf17459
Dhwani Gupta <81840560+dhwani-gupta@users.noreply.github.com>
2026-07-25 20:05:12 +0530

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        //2 pointers
+
+        let sell = 1;
+        let buy = 0;
+        let profit = 0;
+
+        while (sell < prices.length) {
+            if (prices[sell] < prices[buy]) {
+                buy = sell;
+            } else {
+                let currentProfit = (prices[sell] - prices[buy])
+                profit = Math.max(profit, currentProfit);
+            }
+            sell++;
+        }
+
+        return profit;
+
+
+
+
+    }
+}
